5814d491a24b3cf4bccbdb5fdfa43f6e79aba1a1
Edward Whang <ediwhang@gmail.com>
2013-12-07 17:10:20 -0800

    added gamescreen and worldrenderer

---
+++ angelgame/src/com/cal/angelgame/GameScreen.java
@@ -0,0 +1,77 @@
+package com.cal.angelgame;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+
+
+public class GameScreen implements Screen{
+	static final int GAME_READY = 0;
+	static final int GAME_RUNNING = 1;
+	static final int GAME_PAUSED = 2;
+	static final int GAME_LEVEL_END = 3;
+	static final int GAME_OVER = 4;
+	
+	int currstate;//current state
+	Game game;
+	
+	//copypasta
+	OrthographicCamera guiCam;
+	Vector2 touchPoint;
+	SpriteBatch batcher;
+	World world;
+	WorldListener worldListener;
+	WorldRenderer renderer;
+	
+	public GameScreen(Game game) {
+		this.game = game;
+		
+		currstate = GAME_READY;
+		guiCam = new OrthographicCamera(320, 480);
+		guiCam.position.set(320 / 2, 480 / 2, 0);
+		touchPoint = new Vector2();
+		batcher = new SpriteBatch();
+		
+	}
+
+	public void render(float num){}
+	
+	public void resume(){}
+	
+	public void dispose(){}
+	
+	public void show(){}
+	
+	public void hide(){}
+	
+	public void resize(int num1, int num2){}
+	
+	public void pause(){}
+	
+	public void update(float deltaTime){
+		//lag control
+		if (deltaTime > 0.1f) deltaTime = 0.1f;
+		
+		switch (currstate) {
+		case GAME_READY:
+			updateReady();
+			break;
+		case GAME_RUNNING:
+			updateRunning(deltaTime);
+			break;
+		case GAME_PAUSED:
+			updatePaused();
+			break;
+		case GAME_LEVEL_END:
+			updateLevelEnd();
+			break;
+		case GAME_OVER:
+			updateGameOver();
+			break;
+		}
+	}
+}
---
+++ angelgame/src/com/cal/angelgame/WorldRenderer.java
@@ -0,0 +1,13 @@
+package com.cal.angelgame;
+
+public class WorldRenderer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
